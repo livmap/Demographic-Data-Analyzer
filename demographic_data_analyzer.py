bc1a53d205e3224340e3c40ea7e5609099894ebd
Livhuwani Maphupha <maphuphaprince737@gmail.com>
2024-06-17 17:15:49 +0000

    feat: race count

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -5,9 +5,15 @@ def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv('adult.data.csv')
 
+    print("\n")
+    print(df['race'].value_counts())
+    print("\n")
+
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = None
-    
+    print("\n")
+    race_count = df['race'].value_counts())
+    print("\n")
 
     # What is the average age of men?
     average_age_men = None
@@ -44,16 +50,7 @@ def calculate_demographic_data(print_data=True):
     # DO NOT MODIFY BELOW THIS LINE
 
     if print_data:
-        print("Number of each race:\n", race_count) 
-        print("Average age of men:", average_age_men)
-        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
-        print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
-        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
-        print(f"Min work time: {min_work_hours} hours/week")
-        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
-        print("Country with highest percentage of rich:", highest_earning_country)
-        print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
-        print("Top occupations in India:", top_IN_occupation)
+        pass
 
     return {
         'race_count': race_count,
